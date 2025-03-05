633917a887dd3650b26fc4cbfb63ed548edece07
Thomas Clark <thomas.c.clark.27@dartmouth.edu>
2025-03-05 11:06:53 -0500

    Revert "Pulling main to ai-integration"
    This reverts commit c0ba9810b222f49738d4322947e6fa1c51f63aa6, reversing
    changes made to eb1ce509daa7c73fde00d77c6d86de26b63055fd.
    
    newest merge from main deleted ai integration

--- src/router.js
+++ src/router.js
@@ -5,9 +5,9 @@ import mongoose from 'mongoose';
 import * as Thoughts from './controllers/thoughts_controller';
 import { deleteThoughtById, deleteAllThoughts } from './controllers/thoughts_controller';
 import {
-  addFunFact, getCountryDetails, getCountryFacts, getCountryThoughts, getAllCountries,
+  addUserFunFact, getCountryDetails, getCountryUserFacts, getCountryThoughts, getAllCountries,
   getThoughtCoordinates,
-  getAllCountriesWithThoughts, getScratchDataForUser, saveScratchDataForUser,
+  getAllCountriesWithThoughts, getScratchDataForUser, saveScratchDataForUser, generateCountryData,
 } from './controllers/country_controller';
 
 import * as UserController from './controllers/user_controller';
@@ -25,6 +25,7 @@ router.get('/', (req, res) => {
 /**
  * Gets all thoughts.
  */
+
 router.route('/thought')
   .get(async (req, res) => {
     try {
@@ -196,7 +197,7 @@ router.route('/countries/:countryName/funfact')
       }
 
       const { fact } = req.body;
-      const updatedCountry = await addFunFact(countryName, fact);
+      const updatedCountry = await addUserFunFact(countryName, fact);
       res.status(200).json({ message: `Successfully added new fact to ${countryName}`, country: updatedCountry });
     } catch (error) {
       res.status(400).json({ error: `${error.message}` });
@@ -206,7 +207,7 @@ router.route('/countries/:countryName/funfact')
 /**
  * Retrieve all countries unlocked by the authenticated user.
  */
-router.route('/countries/unlocked/all')
+router.route('/countries/unlocked/:id')
   .get(requireAuth, async (req, res) => {
     try {
       const user = await User.findById(req.user.id);
@@ -274,7 +275,7 @@ router.route('/countries/:countryName/funFacts')
         return res.status(403).json({ error: `Access denied. You must unlock ${countryName} first.` });
       }
 
-      const allFacts = await getCountryFacts(countryName);
+      const allFacts = await getCountryUserFacts(countryName);
       res.status(200).json({ message: 'Successfully retrieved all country facts', funFacts: allFacts });
     } catch (error) {
       res.status(500).json({ error: `${error.message}` });
@@ -328,14 +329,17 @@ router.delete('/thoughts/all', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
 /**
  * Sign in a user and return an authentication token.
  */
 router.post('/signin', requireSignin, async (req, res) => {
   try {
-    const { token, email, homeCountry } = UserController.signin(req.user);
-    res.json({ token, email, homeCountry });
+    const {
+      token, id, email, homeCountry, fullName,
+    } = UserController.signin(req.user);
+    res.json({
+      token, id, email, homeCountry, fullName,
+    });
   } catch (error) {
     res.status(422).send({ error: error.toString() });
   }
@@ -346,8 +350,12 @@ router.post('/signin', requireSignin, async (req, res) => {
  */
 router.post('/signup', async (req, res) => {
   try {
-    const { token, email, homeCountry } = await UserController.signup(req.body);
-    res.json({ token, email, homeCountry });
+    const {
+      token, id, email, homeCountry, fullName,
+    } = await UserController.signup(req.body);
+    res.json({
+      token, id, email, homeCountry, fullName,
+    });
   } catch (error) {
     res.status(422).send({ error: error.toString() });
   }
@@ -374,5 +382,9 @@ router.route('/countries/:countryName/scratch')
       res.status(500).json({ error: error.message });
     }
   });
+
+/** ******* OpenAI Routes ********** */
+// for a given country, generate an AI description and fun facts
+router.post('/countries/:countryName/generate-data', requireAuth, generateCountryData);
 
 export default router;
